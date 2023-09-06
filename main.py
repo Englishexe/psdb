a42f6753ce70438d52506c06bfbf4479d1e6290a
Englishexe <dexter_p_turner@icloud.com>
2023-09-06 22:29:34 +0100

    Removed PSDB.settings

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # DEVELOPER FILE
 # Will be removed in V1
 import pythondatabase
-pythondatabase.settings(True, True, True, True)
+pythondatabase.settings(True, True, True, False)
 pythondatabase.start()
